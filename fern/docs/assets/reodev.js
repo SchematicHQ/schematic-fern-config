fa22624d286bbf507dfdc5991de5af72d7306a9f
ryan echternacht <ryan@echternacht.org>
2025-09-05 16:40:34 -0400

    Add reo.dev listening code (#182)

---
+++ fern/docs/assets/reodev.js
@@ -0,0 +1,1 @@
+!function(){var e,t,n;e="5930a4eb486171e",t=function(){Reo.init({clientID:"5930a4eb486171e"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();
